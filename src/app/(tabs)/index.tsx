8756783c0f86e00a484963eefcbae02f93f79481
Pastendi Sherpa <pastendisherpa26@gmail.com>
2024-05-26 14:23:20 +0100

    design item compoenent in index

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,20 +1,37 @@
-import { StyleSheet } from 'react-native'
-
+import { StyleSheet, Image } from 'react-native'
+import products from '@/assets/data/products'
 import EditScreenInfo from '@/src/components/EditScreenInfo'
 import { Text, View } from '@/src/components/Themed'
+import Colors from '@/src/constants/Colors'
 
+const product = products[5]
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text>Project started</Text>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
   },
 })
